1bc856e9137dce8b1da65a6e12a6a0caf53f8b27
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-07-26 01:18:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+const plants = [{
+        "id": 1,
+        "name": "Bamboo",
+        "category": "indoor",
+        "image": "https://vaaree.com/products/faux-evergreen-bamboo-plant-with-pot-dark-green?pr_prod_strat=e5_desc&pr_rec_id=a769795a9&pr_rec_pid=8593226170614&pr_ref_pid=8593226137846&pr_seq=uniform",
+        "price": "500",
+        "description": "Bamboo grows well both indoors and outdoors. If you're growing lucky bamboo in water, you can train it to twist and curl in different ways. You can also arrange different types of bamboo plants together. For example, try potting green and variegated bamboo stalks together."
+    },            //This is a temporary DataBase
+    {
+        "id": 2,
+        "name": "Jasmin",
+        "category": "indoor",
+        "price": "250",
+        "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRMPWirbmwewB6MWH0XdKlOVTUza1YtLhdcobbMn34S5cCzgGDLNF5T4of-_Q2xYA2nzIJ5XYzPr2wNoxCUmLjJ3BIwqLe6hwtQIw2PS3gz&usqp=CAc",
+        "description": "Jasmine is a genus of shrubs and vines in the olive family of Oleaceae. It contains around 200 species native to tropical and warm temperate regions of Eurasia, Africa, and Oceania. Jasmines are widely cultivated for the characteristic fragrance of their flowers."
+    },
+    {
+        "id": 3,
+        "name": "Rose",
+        "category": "indoor",
+        "price": "300",
+        "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRMPWirbmwewB6MWH0XdKlOVTUza1YtLhdcobbMn34S5cCzgGDLNF5T4of-_Q2xYA2nzIJ5XYzPr2wNoxCUmLjJ3BIwqLe6hwtQIw2PS3gz&usqp=CAc",
+        "description": "Roses are a group of perennial and deciduous shrubs."
+    },
+    {
+        "id": 4,
+        "name": "Lily",
+        "category": "indoor",
+        "price": "200",
+        "image":"https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRMPWirbmwewB6MWH0XdKlOVTUza1YtLhdcobbMn34S5cCzgGDLNF5T4of-_Q2xYA2nzIJ5XYzPr2wNoxCUmLjJ3BIwqLe6hwtQIw2PS3gz&usqp=CAc",
+        "description": "Lilies are a genus of flowering plants which includes around 100 species."
+    }
+]
